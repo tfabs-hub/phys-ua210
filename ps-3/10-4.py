31dfe6a6988f49a761e90500e726aa5a22f5c417
tfabs-hub <64419030+tfabs-hub@users.noreply.github.com>
2023-09-26 17:04:04 -0400

    Update 10-4.py

--- ps-3/10-4.py
+++ ps-3/10-4.py
@@ -1,8 +1,24 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 26 16:57:44 2023
 
-@author: tommyfabian
-"""
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 1000
+T_half = 3.053
+T_half_seconds = T_half * 60
+lambda_ = np.log(2) / T_half_seconds
+
+
+
+uniformNumbers = np.random.uniform(size=N)
+decayTimes = -np.log(1 - uniformNumbers) / lambda_
+sortedDecayTimes = np.sort(decayTimes)
+notDecayed = np.arange(N, 0, -1)
+
+
+plt.plot(sortedDecayTimes, notDecayed)
+plt.xlabel('Time (s)')
+plt.ylabel('Number of Atoms Not Decayed')
+plt.title('Decay of Atoms Over Time')
+plt.grid(True)
+plt.show()
